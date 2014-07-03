930ca47b10a259a3a99d5f45eb813daa7258cc33
Michael Meluso <melusom2@tcnj.edu>
2014-07-03 03:58:08 +0000

    added rdio library files and code for playlist retrieval

---
+++ getPlaylists.php
@@ -0,0 +1,27 @@
+<html>
+  <head>
+     <title>PHP Test</title>
+  </head>
+  <body>
+     <?php
+       session_start();
+
+       require_once '../rdio.php';
+       require_once 'rdio-consumer-credentials.php';
+       
+       # create an instance of the Rdio object with our consumer credentials
+       $rdio = new Rdio(array(RDIO_CONSUMER_KEY, RDIO_CONSUMER_SECRET));
+     
+       # authenticate against the Rdio service
+       $url = $rdio->begin_authentication('oob');
+       print "Go to: $url\n";
+       print "Then enter the code: ";
+       $verifier = trim(fgets(STDIN));
+       $rdio->complete_authentication($verifier);
+
+       # find out what playlists you created
+       $myPlaylists = $rdio->call('getPlaylists')->result->owned;
+     
+     ?>
+  </body>
+</html>
---
+++ rdio-consumer-credentials.php
@@ -0,0 +1,5 @@
+<?php
+# you can get these by signing up for a developer account at:
+# http://developer.rdio.com/
+define('RDIO_CONSUMER_KEY', 'LqyMdxo_cvPGvVhAGafLvA');
+define('RDIO_CONSUMER_SECRET', 'jzepAFYP3FvEGpYpijdHbQ');
